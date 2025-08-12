b6656b9ba74506d164639d310ce985af34322a17
Hrishikesh <hrishikeshreddy2006@gmail.com>
2025-08-12 14:07:34 +0530

    Add files via upload

---
+++ ConcatenateStrings.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ConcatenateStrings {
+	public static void main(String[] args) {
+        String first = "Hello";
+        String second = "World";
+        String result = first + " " + second;
+        System.out.println("Concatenated: " + result);
+    }
+
+}
---
+++ TrimWhitespace.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class TrimWhitespace {
+	 public static void main(String[] args) {
+	        String s = "  Hello  ";
+	        System.out.println("[" + s.trim() + "]");     
+	    }
+
+}
